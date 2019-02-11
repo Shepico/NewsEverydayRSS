eacfb99551f4bc1994886d0942aa73c13df903a8
Shepico <Shepico@gmail.com>
2019-02-11 14:49:57 +0400

    Подключил Hibernate

--- src/ru/shepico/utils/StaticUtils.java
+++ src/ru/shepico/utils/StaticUtils.java
@@ -10,6 +10,7 @@ public class StaticUtils {
     public static final int WIDTH_PRG = 325;
     public static final int HEIGHT_PRG = 700;
     public static final String VERSION = "0.1.190211";
+    public static final String BUG = "01"; // От пользователей
     public static final String NAME_PRG = "News RSS";
     //
     private static java.util.Locale locale = java.util.Locale.US;
